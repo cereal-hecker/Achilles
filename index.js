c4ef3ea5d2ec621582c3570e25a8eec02600be2a
cereal-hecker <91269087+cereal-hecker@users.noreply.github.com>
2022-09-04 11:04:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,41 @@
 
 var genre = $("body").attr("id");
 var i = 2
+var flag = 0
+
 
 $(".interest-icon").click(function(){
-
     var choice = $(this).attr("id")
-    if (i === 4){
+    if (i >= 4){
         togglePremiumPopup()
         $("#overlay").css("display", "block")
+        flag = 2
     }
     else if (choice == "like"){
         toggleMatchPopup()
         $("#overlay").css("display", "block")
+        flag = 1
     }
     else if (choice == "hate"){
         $(".book-image").attr("src", "images/" + genre + "/" + genre + i +".jpg")
         console.log(i)
         i += 1
     }
+    console.log("flag = " + flag + ", i = " + i)
 })
 
 
-$(".close-premium-popup").click(function(){
-    togglePremiumPopup()
-})
-$(".close-match-popup").click(function(){
-    toggleMatchPopup()
-})
+
+    $("div.close-match-popup").click(function(){
+        toggleMatchPopup()
+    })
+
+
+    $("div.close-premium-popup").click(function(){
+        togglePremiumPopup()
+    })
+
+
 
 function toggleMatchPopup() {
     $(".match-popup").toggle();
